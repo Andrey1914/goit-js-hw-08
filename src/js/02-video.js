74bbba131b5a6226a16925905316e2b104ba2d7a
Andrey1914 <anlikajw@gmail.com>
2022-06-03 23:10:09 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,45 @@
+import VimeoPlayer from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onPlay = function (data) {
+  let video = document.querySelector('iframe');
+
+  // если localStorage содержит значение currentTime (текущее время), присваиваем это значение video.currentTime
+  if (localStorage.currentTime) {
+    video.currentTime = localStorage.currentTime;
+  }
+
+  // при каждом изменении video.currentTime, записываем его значение в localStorage.currentTime
+  video.addEventListener(
+    'timeupdate',
+    () => (localStorage.currentTime = video.currentTime)
+  );
+};
+
+player.on('play', onPlay);
+
+// window.onload = () => {
+//   // находим элемент <video>
+//   let video = document.querySelector('#vimeo-player');
+
+//   // если localStorage содержит значение currentTime (текущее время), присваиваем это значение video.currentTime
+//   if (localStorage.currentTime) {
+//     video.currentTime = localStorage.currentTime;
+//   }
+
+//   // при каждом изменении video.currentTime, записываем его значение в localStorage.currentTime
+//   video.addEventListener(
+//     'timeupdate',
+//     () => (localStorage.currentTime = video.currentTime)
+//   );
+// };
